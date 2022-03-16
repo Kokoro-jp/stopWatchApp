ed0ab9db9611da7d81bdf612503df27e2b69ff0f
EC2 Default User <ec2-user@ip-172-31-33-83.ap-northeast-3.compute.internal>
2022-03-16 13:35:30 +0000

    修正3回目

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-/*global ture*/
 
 const timer = document.getElementById("timer");
 
@@ -9,7 +8,6 @@ const reset = document.getElementById("reset");
 let startTime;  //Startボタンクリック時の時刻
 let elapsedTime = 0;  //StartからStopまでの経過時間
 let timeoutid; //ID
-
 
 function countUp(){
   const d = new Date(Date.now() - startTime + elapsedTime);
@@ -26,9 +24,7 @@ function countUp(){
     
     countUp();
   }, 10);
-  
 }
-
 
 // 活性非活性（初期状態・リセット状態）
 function setButtonStateInitial() {
@@ -48,7 +44,6 @@ function setButtonStateStoped() {
   stop.classList.add('inactive'); //非活性
   reset.classList.remove('inactive'); //活性
 }
-
 
 // スタートボタン）
 start.addEventListener("click", () => {
@@ -80,14 +75,10 @@ reset.addEventListener("click", () => {
    clearTimeout(timeoutid);
    elapsedTime = 0;
    timer.textContent = '0:0:0:0';
-  // startTime = Date.now();
-  // countUp();
-   
    }else{
   // タイマー停止中なら初期値にリセット
   setButtonStateInitial();
   timer.textContent = '0:0:0:0';
   elapsedTime = 0;
    }
-  
 });
